249d6d7107e0dae5e1f3a29647eecfd44e1eee6a
luciano_v96 <lucianovenini@gmail.com>
2021-03-14 03:58:29 -0300

    Encrypted pass

---
+++ src/main/kotlin/com/poketeammaker/config/AppConfigForJasyptStarter.kt
@@ -0,0 +1,8 @@
+package com.poketeammaker.config
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableEncryptableProperties
+class AppConfigForJasyptStarter
